b8fc1d2669f94d73d4cb81483252ce385865fbbd
Patrick <pschmidt@pasit.nl>
2025-12-02 08:41:55 +0000

    day2

--- Exercises/Exercise 4/Starter/Calculator/CalculatorApp.cs
+++ Exercises/Exercise 4/Starter/Calculator/CalculatorApp.cs
@@ -14,29 +14,11 @@ public partial class CalculatorApp : Form
         if (int.TryParse(txtA.Text, out int a) && int.TryParse(txtB.Text, out int b))
         {
 
-            //Task.Run(() => LongAdd(a, b))
-            //    .ContinueWith(pt => { 
-            //        _main?.Send(UpdateAnswer, pt.Result);
-            //    }) ;
-            //int result = LongAdd(a, b);
-           // UpdateAnswer(result);
-
-           // int result = await LongAddAsync(a, b);
-            //UpdateAnswer(result);
-
-            // Dead lock
-            //Task<int> t1 = DoeIets(a, b);
-            //int result = t1.Result;
-
-            int result = await DoeIets(a, b);
+            int result = LongAdd(a, b);
             UpdateAnswer(result);
         }
     }
 
-    private async Task<int> DoeIets(int a, int b)
-    {
-        return await LongAddAsync(a, b);
-    }
     private void UpdateAnswer(object? result)
     {
         lblAnswer.Text = result?.ToString();
@@ -46,9 +28,5 @@ public partial class CalculatorApp : Form
     {
         Task.Delay(10000).Wait();
         return a + b;
-    }
-    private Task<int> LongAddAsync(int a, int b)
-    {
-        return Task.Run(() => LongAdd(a, b));
     }
 }
---
+++ Live/Module_3/Draadjes/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace Draadjes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Synchroon();
+        //ViaThread();
+        //ViaThreadPool();
+        // ViaDelegates();
+        ViaTasks();
+
+        Console.WriteLine("Einde programma");
+       Console.ReadLine();
+    }
+
+    private static void ViaTasks()
+    {
+
+        Task t1 = new Task(() =>
+        {
+            var result = LongAdd(3, 4);
+            Console.WriteLine($"Het resultaat is {result}");
+        });
+
+        t1.Start();
+
+
+        Task t2 = Task.Run(() =>
+        {
+            var result = LongAdd(3, 5);
+            Console.WriteLine($"Het resultaat is {result}");
+        });
+
+
+        var t3 = Task.Run(() => LongAdd(9, 5)).ContinueWith(previousTask =>
+        {
+            int result = previousTask.Result;
+
+            Console.WriteLine($"Het resultaat is {result}");
+        });
+        t3.ContinueWith(pt => Console.WriteLine("Doen we ook"));
+        
+    }
+
+    private static void ViaDelegates()
+    {
+        Func<int, int, int> del = LongAdd;
+
+        IAsyncResult ar = del.BeginInvoke(5, 6, ar => {
+            int result = del.EndInvoke(ar);
+            Console.WriteLine(result);
+        }, null);  
+    }
+
+    private static void ViaThreadPool()
+    {
+        ThreadPool.QueueUserWorkItem((o)=>Synchroon());
+    }
+
+    private static void ViaThread()
+    {
+       
+        ThreadStart start = new ThreadStart(Synchroon);
+        Thread th = new Thread(start);
+        
+        th.Start();
+    }
+
+    private static void Synchroon()
+    {
+        var result = LongAdd(3, 4);
+        Console.WriteLine($"Het resultaat is {result}");
+    }
+
+    static int LongAdd(int a, int b)
+    {
+        Task.Delay(5000).Wait();
+        return a + b;
+    }
+}
