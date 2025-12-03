1a49bdec3cf42acaceb3b7f2539b3a6d2e9ade7b
Patrick <pschmidt@pasit.nl>
2025-12-03 14:08:57 +0000

    day3

---
+++ Live/Module_6/Confidentiality/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Confidentiality;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //TestSymmetrisch();
+        TestAsymmetrisch();
+    }
+
+
+    static string publicKey;
+   
+    private static void TestAsymmetrisch()
+    {
+       var dsa =  GeneratePublicKeyForSender();
+        byte[] crypto = SenderAsymmetrisch();
+        Console.WriteLine(Encoding.UTF8.GetString(crypto));
+        OntvangerAsymmetrisch(dsa, crypto);
+    }
+
+    private static RSA GeneratePublicKeyForSender()
+    {
+        RSA alg = RSA.Create();
+        publicKey = alg.ToXmlString(false);
+        return alg;
+       
+    }
+
+    private static void OntvangerAsymmetrisch(RSA alg,  byte[] crypto)
+    {
+        var cip = alg.Decrypt(crypto, RSAEncryptionPadding.Pkcs1);
+        Console.WriteLine(Encoding.UTF8.GetString(cip));
+    }
+
+    private static byte[] SenderAsymmetrisch()
+    {
+        string document = "Hello World!";
+        RSA alg = RSA.Create();
+        alg.FromXmlString(publicKey);
+        return alg.Encrypt(Encoding.UTF8.GetBytes(document), RSAEncryptionPadding.Pkcs1);
+    }
+
+    private static void TestSymmetrisch()
+    {
+        byte[] crypto = SenderSymmetrisch();
+        Console.WriteLine(Encoding.UTF8.GetString(crypto));
+        OntvangerSymmetrisch(crypto);
+    }
+
+    static byte[] key;
+    static byte[] iv;
+
+    private static void OntvangerSymmetrisch(byte[] crypto)
+    {
+        Aes algorithm = Aes.Create();
+        algorithm.Mode = CipherMode.CBC;
+        algorithm.Key = key;
+        algorithm.IV = iv;
+
+        using (var mem = new MemoryStream(crypto))
+        {
+            using (var crypt = new CryptoStream(mem, algorithm.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                using (var reader = new StreamReader(crypt))
+                {
+                    string document = reader.ReadToEnd();
+                    Console.WriteLine(document);
+                }
+            }
+        }
+    }
+
+    private static byte[] SenderSymmetrisch()
+    {
+        string document = "Hello World!";
+
+        Aes algorithm = Aes.Create();
+        algorithm.Mode = CipherMode.CBC;
+        key = algorithm.Key;
+        iv = algorithm.IV;
+
+        using (var mem = new MemoryStream())
+        {
+            using (var crypt = new CryptoStream(mem, algorithm.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using (var writer = new StreamWriter(crypt))
+                {
+                    writer.Write(document);
+                }
+            }
+
+            return mem.ToArray();
+        }
+    }
+}
---
+++ Live/Module_6/Integrity/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Integrity;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        DSA dsa = DSA.Create();
+        Console.WriteLine(dsa.ToXmlString(false));
+        Console.WriteLine(new string('=', 80));
+        Console.WriteLine(dsa.ToXmlString(true));
+        // TestHash();
+        //TestSymmetrisch();
+        TestAsymmetrisch();
+
+    }
+
+    private static void TestAsymmetrisch()
+    {
+        (byte[] Signature, string Document, string PublicKey) result = SenderAsymmetrisch();
+        Console.WriteLine(Convert.ToBase64String(result.Signature));
+        Console.WriteLine(result.Document);
+        result.Document += "!"; // Ed in de bocht
+        OntvangerAsymmetrisch(result);
+    }
+
+    private static void TestSymmetrisch()
+    {
+        (byte[] Hash, string Document) result = SenderSymmetrisch();
+        Console.WriteLine(Convert.ToBase64String(result.Hash));
+        Console.WriteLine(result.Document);
+        //result.Document += "!"; // Ed in de bocht
+        OntvangerSymmetrisch(result);
+    }
+
+    private static void TestHash()
+    {
+        (byte[] Hash, string Document) result = SenderSymmetrisch();
+        Console.WriteLine(Convert.ToBase64String(result.Hash));
+        Console.WriteLine(result.Document);
+        result.Document += "!"; // Ed in de bocht
+        OntvangerSymmetrisch(result);
+        ;
+    }
+
+    private static void OntvangerAsymmetrisch((byte[] Signature, string Document, string PublicKey) result)
+    {
+        SHA1 sha1 = SHA1.Create();
+        byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(result.Document));
+        DSA dsa = DSA.Create();
+        dsa.FromXmlString(result.PublicKey);
+        bool isOk = dsa.VerifySignature(hash, result.Signature);
+        Console.WriteLine(isOk ? "Prima. Origineel":"Hier is aan gemorreld");
+    }
+
+    private static (byte[] Signature, string Document, string PublicKey) SenderAsymmetrisch()
+    {
+        string document = "Hello World!";
+
+        SHA1 sha1 = SHA1.Create();
+        byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(document));
+
+        DSA dsa = DSA.Create();
+        string publicKey = dsa.ToXmlString(false);
+        byte[] signature = dsa.CreateSignature(hash);
+
+        return (signature, document, publicKey);
+    }
+    private static void OntvangerSymmetrisch((byte[] Hash, string Document) result)
+    {
+        HMACSHA256 hmac = new HMACSHA256();
+        hmac.Key = Encoding.UTF8.GetBytes("Pa$$w0rd");
+
+        byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(result.Document));
+        Console.WriteLine(Convert.ToBase64String(hash));
+        Console.WriteLine(Convert.ToBase64String(result.Hash));
+    }
+
+    private static (byte[] Hash, string Document) SenderSymmetrisch()
+    {
+        string document = "Hello World!";
+
+        HMACSHA256 hmac = new HMACSHA256();
+        hmac.Key = Encoding.UTF8.GetBytes("Pa$$w0rd");
+        byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(document));
+        
+        return (hash, document);
+    }
+    private static void OntvangerHash((byte[] Hash, string Document) result)
+    {
+        SHA1 sha1 = SHA1.Create();
+        byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(result.Document));
+        Console.WriteLine(Convert.ToBase64String(hash));
+        Console.WriteLine(Convert.ToBase64String(result.Hash));
+    }
+
+    private static (byte[] Hash, string Document) SenderHash()
+    {
+        string document = "Hello World!";
+
+        SHA1 sha1 = SHA1.Create();
+        byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(document));
+
+        return (hash, document);
+    }
+}
